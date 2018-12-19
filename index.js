6255258505d025c258d9fc46f9283cc6203f0549
Sam Dutton <samdutton@gmail.com>
2018-12-19 17:34:56 +0000

    Write URL, category title and score

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const lighthouse = require('lighthouse');
 const log = require('lighthouse-logger');
 
+const OUTPUT_FILEPATH = 'lhr.json';
+
 function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
   return chromeLauncher.launch(flags).then(chrome => {
     flags.port = chrome.port;
@@ -20,11 +22,14 @@ const flags = {
 
 const url = 'https://github.com';
 launchChromeAndRunLighthouse(url, flags).then(results => {
-  console.log('results.lhr.categories', results.lhr.categories);
-  // results.lhr.categories.forEach(category => {
-  //   console.log(category.title, category.score);
-  //   fs.appendFileSync("lhr.json", `${url}, ${category.title}, ${category.score}\n`);
-  // });
-  // console.log('Lighthouse results:', results.lhr);
+    fs.writeFile(OUTPUT_FILEPATH, '', 
+      () => {console.log('Deleted old file contents')});
+    // console.log('results.lhr.categories', results.lhr.categories);
+    const categories = Object.values(results.lhr.categories);
+    categories.forEach(category => {
+        console.log(category.title, category.score);
+        fs.appendFileSync(OUTPUT_FILEPATH, 
+          `${url}, ${category.title}, ${category.score}\n`);
+  });
 });
 
