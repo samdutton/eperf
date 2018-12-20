2dc212dc0b8e2776f11a2d790db9f8bfa878905e
Sam Dutton <samdutton@gmail.com>
2018-12-20 14:13:19 +0000

    Save only URL and scores for site

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const chromeLauncher = require('chrome-launcher');
 const fs = require('fs');
 const lighthouse = require('lighthouse');
-const log = require('lighthouse-logger');
+// const log = require('lighthouse-logger');
 
 const OUTPUT_FILEPATH = 'lhr.json';
 
@@ -15,21 +15,26 @@ function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
 
 const flags = {
   chromeFlags: ['--headless'],
-  logLevel: 'info', 
+  logLevel: 'info'
 };
 
-//log.setLevel(flags.logLevel);
+// log.setLevel(flags.logLevel);
 
 const url = 'https://github.com';
-launchChromeAndRunLighthouse(url, flags).then(results => {
-    fs.writeFile(OUTPUT_FILEPATH, '', 
-      () => {console.log('Deleted old file contents')});
-    // console.log('results.lhr.categories', results.lhr.categories);
-    const categories = Object.values(results.lhr.categories);
-    categories.forEach(category => {
-        console.log(category.title, category.score);
-        fs.appendFileSync(OUTPUT_FILEPATH, 
-          `${url}, ${category.title}, ${category.score}\n`);
-  });
+let allSiteResults = [];
+fs.writeFile(OUTPUT_FILEPATH, '', () => {
+  console.log('Deleted old output file contents');
 });
+
+launchChromeAndRunLighthouse(url, flags).then(results => {
+  let siteResults = [`${url}`];
+  const categories = Object.values(results.lhr.categories);
+  for (let category of categories) {
+    siteResults.push(`${category.score}`);
+  }
+  allSiteResults.push(siteResults.join(','));
+  console.log(allSiteResults.join('\n'));
+  fs.appendFileSync(OUTPUT_FILEPATH, allSiteResults.join('\n'));
+});
+
 
