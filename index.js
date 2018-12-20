c1020702dc9a4a7c710ee6e96a4b7870971b864e
Sam Dutton <samdutton@gmail.com>
2018-12-20 15:27:33 +0000

    Added handling for runtime errors

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const OUTPUT_FILEPATH = 'lhr.json';
 function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
   return chromeLauncher.launch(flags).then(chrome => {
     flags.port = chrome.port;
-    return lighthouse(url, flags, config).then(results =>
-      chrome.kill().then(() => results));
+    return lighthouse(url, flags, config)
+      .then(results => chrome.kill().then(() => results));
   });
 }
 
@@ -20,21 +20,31 @@ const flags = {
 
 // log.setLevel(flags.logLevel);
 
-const url = 'https://github.com';
+const name = 'John Lewis';
+const type = 'home';
+const url = 'https://johnlewis.com';
+
 let allSiteResults = [];
 fs.writeFile(OUTPUT_FILEPATH, '', () => {
   console.log('Deleted old output file contents');
 });
 
 launchChromeAndRunLighthouse(url, flags).then(results => {
-  let siteResults = [`${url}`];
-  const categories = Object.values(results.lhr.categories);
-  for (let category of categories) {
-    siteResults.push(`${category.score}`);
+  // fs.appendFileSync(OUTPUT_FILEPATH, JSON.stringify(results));
+  // console.log('>>>>>', JSON.stringify(results));
+  const runtimeErrorMessage = results.lhr.runtimeError.message;
+  if (runtimeErrorMessage) {
+    console.log(`Runtime error for ${url}`);
+  } else {
+    let siteResults = [`${name}`,`${type}`,`${url}`];
+    const categories = Object.values(results.lhr.categories);
+    for (let category of categories) {
+      siteResults.push(`${category.score}`);
+    }
+    allSiteResults.push(siteResults.join(','));
+    // console.log(allSiteResults.join('\n'));
+    fs.appendFileSync(OUTPUT_FILEPATH, allSiteResults.join('\n'));
   }
-  allSiteResults.push(siteResults.join(','));
-  console.log(allSiteResults.join('\n'));
-  fs.appendFileSync(OUTPUT_FILEPATH, allSiteResults.join('\n'));
 });
 
 
