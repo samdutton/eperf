f7627d5fe940ae4ce3f57c11e499db1af10c6238
Sam Dutton <samdutton@gmail.com>
2018-12-20 19:18:55 +0000

    Added input.csv and output.csv and a README, plus comments

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const fs = require('fs');
 const lighthouse = require('lighthouse');
 // const log = require('lighthouse-logger');
 
+const FLAGS = {
+  chromeFlags: ['--headless'],
+  logLevel: 'info'
+};
+
 const INPUT = 'input.csv';
 const OUTPUT = 'output.csv';
 
+// Delete existing output data.
+fs.writeFile(OUTPUT, '', () => {
+  console.log('Deleted existing output data');
+});
+
+// Get page data from CSV file INPUT and run an audit for each page.
+// Each line in INPUT begins with a URL followed (optionally) by other CSV data.
+// For example: https://johnlewis.com,John Lewis,homepage
+const inputFileText = fs.readFileSync(INPUT, 'utf8');
+const allPageData = inputFileText.split('\n');
+for (let pageData of allPageData) {
+  if (pageData !== '') {
+    audit(pageData);
+  }
+}
+
+// Launch Chrome, run a Lighthouse audit, then kill Chrome.
 function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
   return chromeLauncher.launch(flags).then(chrome => {
     flags.port = chrome.port;
@@ -14,28 +36,14 @@ function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
   });
 }
 
-const flags = {
-  chromeFlags: ['--headless'],
-  logLevel: 'info'
-};
-
 // log.setLevel(flags.logLevel);
 
-fs.writeFile(OUTPUT, '', () => {
-  console.log('Deleted old output file contents');
-});
-
-var inputFileText = fs.readFileSync(INPUT, 'utf8');
-const allPageData = inputFileText.split('\n');
-for (let pageData of allPageData) {
-  if (pageData !== '') {
-    audit(pageData);
-  }
-}
-
+// Run a Lighthouse audit for a web page.
+// pageData is a CSV string beginning with a URL.
+// For example: https://johnlewis.com,John Lewis,homepage
 function audit(pageData) {
   const url = pageData.split(',')[0];
-  launchChromeAndRunLighthouse(url, flags).then(results => {
+  launchChromeAndRunLighthouse(url, FLAGS).then(results => {
     const runtimeErrorMessage = results.lhr.runtimeError.message;
     if (runtimeErrorMessage) {
       console.log(`Runtime error for ${url}`);
